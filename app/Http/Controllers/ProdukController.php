de7e943eb6d394af10bc5bf2aac3166934d64332
widiarrohman1234 <widiarrohman1234@gmail.com>
2021-01-04 10:12:02 +0700

    bug fix

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -6,8 +6,8 @@ use App\Models\Produk;
 class ProdukController extends Controller
 {
 	function index(){
-		//$id_user = request()->user()->id;
-		//$data['list_produk'] = Produk::where('id_user', $id_user)->get();
+		// $id_user = request()->user()->id;
+		// $data['list_produk'] = Produk::where('id_user', $id_user)->get();
 		$user = request()->user();
 		$data['list_produk'] = $user->produk;
 		return view('produk.index', $data);
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -58,7 +58,7 @@ class UserController extends Controller
 		return redirect('admin/user')->with('success','Data Berhasil Diubah');
 	}
 
-	function destroy(user $user){
+	function destroy(User $user){
 		$user->delete();
 		return redirect('admin/user')->with('danger','Data Berhasil Dihapus');
 	}
