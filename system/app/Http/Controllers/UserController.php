6a85e95fd741c503349d977d3e566865969f2491
widiarrohman1234 <widiarrohman1234@gmail.com>
2021-01-15 16:23:22 +0700

    error handling Abort

--- system/app/Http/Controllers/UserController.php
+++ system/app/Http/Controllers/UserController.php
@@ -44,6 +44,9 @@ class UserController extends Controller
 	}
 
 	function show(User $user){
+		$loggedUser = request()->user();
+		if($loggedUser->id != $user->id) return abort(403);
+
 		$data['user'] = $user;
 		return view('user.show', $data);
 	}
