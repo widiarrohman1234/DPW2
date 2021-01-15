d5a79e27272215087916def363e97afa9980539e
widiarrohman1234 <widiarrohman1234@gmail.com>
2021-01-15 14:30:33 +0700

    project 13

--- system/app/Http/Controllers/AuthController.php
+++ system/app/Http/Controllers/AuthController.php
@@ -11,25 +11,30 @@ class AuthController extends Controller
 	}
 
 	function loginProcess(User $user){
-		// if(Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
-		// 	return redirect('beranda')->with('success','Login Berhasil, Selamat datang');
-		// } else {
-		// 	return back()->with('danger', 'Login gagal, Silahkan coba kembali');
-		// }
+		if(Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+			$user = Auth::user();
+			if($user->level == 1)return redirect('beranda/admin')->with('success','Login Berhasil, Selamat datang');
+			if($user->level == 2)return redirect('beranda/user')->with('success','Login Berhasil, Selamat datang');
+			if($user->level == 3)return redirect('beranda/penjual')->with('success','Login Berhasil, Selamat datang');
+			if($user->level == 4)return redirect('beranda/pembeli')->with('success','Login Berhasil, Selamat datang');
 
-		if (request('login_as') == 1) {
-			if(Auth::guard('pembeli')->attempt(['email' => request('email'), 'password' => request('password')])){
-				return redirect('beranda/pembeli')->with('success','Login Berhasil, Selamat datang');
-			}else{
-				return back()->with('danger', 'Login gagal, Silahkan coba kembali');
-			}
 		} else {
-			if(Auth::guard('penjual')->attempt(['email' => request('email'), 'password' => request('password')])){
-				return redirect('beranda/penjual')->with('success','Login Berhasil, Selamat datang');
-			}else{
-				return back()->with('danger', 'Login gagal, Silahkan coba kembali');
-			}
+			return back()->with('danger', 'Login gagal, Silahkan coba kembali');
 		}
+
+		// if (request('login_as') == 1) {
+		// 	if(Auth::guard('pembeli')->attempt(['email' => request('email'), 'password' => request('password')])){
+		// 		return redirect('beranda/pembeli')->with('success','Login Berhasil, Selamat datang');
+		// 	}else{
+		// 		return back()->with('danger', 'Login gagal, Silahkan coba kembali');
+		// 	}
+		// } else {
+		// 	if(Auth::guard('penjual')->attempt(['email' => request('email'), 'password' => request('password')])){
+		// 		return redirect('beranda/penjual')->with('success','Login Berhasil, Selamat datang');
+		// 	}else{
+		// 		return back()->with('danger', 'Login gagal, Silahkan coba kembali');
+		// 	}
+		// }
 		
 
 		//dd(request()->all());
